ecf1b049268b3df313d2430c5ac6d1e423d9e240
Shane Bendak <shebah712@gmail.com>
2020-07-15 14:03:58 -0700

    [arrays] Add favorite colors

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Shane";
+console.log(name);
+
+var myLocation = "Las Vegas";
+console.log(myLocation);
+
+var faveNumber = 13;
+console.log(faveNumber);
+
+var colors = ["blue", "green", "red", "orange", "white"];
+console.log(colors);
