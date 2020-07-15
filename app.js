2ad03c79b58c87e87c6d913f022c1789ae84db87
Shane Bendak <shebah712@gmail.com>
2020-07-15 15:45:09 -0700

    [js] added insta post

--- app.js
+++ app.js
@@ -68,3 +68,47 @@ for (let i = 0; i < cars.length; i++) {
     let eachCar = cars[i];
     console.log(eachCar);
 }
+
+//OBJECTS
+// key: value pairs
+
+const shane = {
+    fullName: 'Shane Bendak',
+    age: 27,
+    myLocation: 'Las Vegas',
+    placesTraveled: ["Texas", "Ohio", "Pennsylvania", "New York"]
+}
+
+console.log(shane);
+//myLocation
+console.log(shane.myLocation);
+// specific element out of an array using array index []
+console.log(shane.placesTraveled[2]);
+
+// likes, caption, shares, comments, username, picture
+const myReallyCoolPost = {
+    username: '@generalassembly',
+    likes: 12,
+    comments: ['nice', 'cool', 'js'],
+    caption: 'Day 3 of SEI',
+    picture: 'img link',
+    shares: 2
+}
+console.log(myReallyCoolPost);
+
+myReallyCoolPost.myLocation = 'Las Vegas';
+console.log(myReallyCoolPost);
+
+// instigram post object
+
+const instaPost = {
+    username: '@thatboi',
+    likes: 14,
+    comments: ['Why?', 'What?', 'Who?', 'When?'],
+    caption: 'Buruning down my favorite AppleBees',
+    picture: 'img picture',
+    shares: 3,
+    myLocation: 'Downingtown, PA'
+}
+
+console.log(instaPost);
