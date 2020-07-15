2b7c168e8d8f18e938aaa2b35f45baf0d7f9b155
Shane Bendak <shebah712@gmail.com>
2020-07-15 14:25:03 -0700

    [js] added loops

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ console.log(faveNumber);
 
 var colors = ["blue", "green", "red", "orange", "white"];
 console.log(colors);
+
+// WHILE LOOPS
+var number = 10;
+var otherNumber = 1;
+while (otherNumber < number) {
+    console.log(otherNumber);
+    //otherNumber += 1;
+    otherNumber = otherNumber + 1
+}
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+
+while (myOtherNumber < numberTwo) {
+    console.log(myOtherNumber);
+    myOtherNumber += 1; // adds 1 to myOtherNumber and reruns the loop until the condition is not met
+}
